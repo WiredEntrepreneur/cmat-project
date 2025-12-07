107ca35ef663566846a6d23134737612fdfe50fc
WiredEntrepreneur <raj.somarouthu@googlemail.com>
2025-12-07 23:30:32 +0530

    updated playbook and terraform files

--- main.tf
+++ main.tf
@@ -1,55 +1,74 @@
+#####################
+# Network
+#####################
+
 resource "aws_vpc" "sl-vpc" {
   cidr_block = "10.0.0.0/16"
+
   tags = {
     Name = "sl-vpc"
   }
 }
+
 resource "aws_subnet" "sl-subnet" {
-  vpc_id = aws_vpc.sl-vpc.id
-  cidr_block = "10.0.1.0/24"
-  depends_on = [aws_vpc.sl-vpc]
+  vpc_id                  = aws_vpc.sl-vpc.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+
+  depends_on = [aws_vpc.sl-vpc]
+
   tags = {
     Name = "sl-subnet"
   }
 }
-resource "aws_route_table" "sl-route-table" {
-  vpc_id = aws_vpc.sl-vpc.id
-  tags = {
-    Name = "sl-route-table"
-  }
-}
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.sl-subnet.id
-  route_table_id = aws_route_table.sl-route-table.id
-}
-
-
-
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id     = aws_vpc.sl-vpc.id
+  vpc_id = aws_vpc.sl-vpc.id
+
   depends_on = [aws_vpc.sl-vpc]
+
   tags = {
     Name = "sl-gw"
   }
 }
+
+resource "aws_route_table" "sl-route-table" {
+  vpc_id = aws_vpc.sl-vpc.id
+
+  tags = {
+    Name = "sl-route-table"
+  }
+}
+
 resource "aws_route" "sl-route" {
   route_table_id         = aws_route_table.sl-route-table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 }
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.sl-subnet.id
+  route_table_id = aws_route_table.sl-route-table.id
+}
+
+#####################
+# Security Group
+#####################
+
 variable "sg_ports" {
-  type = list(number)
+  type    = list(number)
   default = [22, 443, 80, 8080]
 }
+
 resource "aws_security_group" "sl-sg" {
   name        = "sl-sg"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  description = "Allow SSH/HTTP/HTTPS inbound and all outbound traffic"
   vpc_id      = aws_vpc.sl-vpc.id
+
   dynamic "ingress" {
     for_each = var.sg_ports
     iterator = ports
+
     content {
       from_port   = ports.value
       to_port     = ports.value
@@ -57,55 +76,64 @@ resource "aws_security_group" "sl-sg" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
-  
 
-
-egress {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+#####################
+# AMI
+#####################
+
 data "aws_ami" "myami" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
+
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm*"]
   }
 }
-resource "aws_instance" "myec2" {
-  ami             = data.aws_ami.myami.id
-  instance_type   = "t2.micro"
-  lifecycle {
-  prevent_destroy = false
-  ignore_changes = [
-    ami,                 # prevent recreation because AMI changed
-    user_data,           # prevent recreation when user_data changes
-    tags,                # prevent recreation if tags change
-  ]
-  }
 
-  key_name        = "web-key"
-  subnet_id       = aws_subnet.sl-subnet.id
-  security_groups = [aws_security_group.sl-sg.id]
+#####################
+# EC2 instance
+#####################
+
+resource "aws_instance" "myec2" {
+  ami           = data.aws_ami.myami.id
+  instance_type = "t2.micro"
+
+  subnet_id              = aws_subnet.sl-subnet.id
+  vpc_security_group_ids = [aws_security_group.sl-sg.id]
+
+  # Use the key pair we created in mykey.tf
+  key_name = aws_key_pair.aws-key.key_name
+
   tags = {
     Name = "terraform-instance"
   }
-   user_data = <<EOF
-  #!/bin/bash
-  mkdir -p /home/ec2-user/.ssh
-  echo "${file("~/.ssh/id_ed25519.pub")}" >> /home/ec2-user/.ssh/authorized_keys
-  chown -R ec2-user:ec2-user /home/ec2-user/.ssh
-  chmod 600 /home/ec2-user/.ssh/authorized_keys
-  EOF
+
+  # Optional: stop AMI refresh from forcing recreation
+  lifecycle {
+    ignore_changes = [
+      ami
+    ]
+  }
 }
+
+#####################
+# Ansible inventory
+#####################
+
 resource "local_file" "inventory" {
+  filename = "${path.module}/inventory.ini"
+
   content = <<EOF
 [webserver]
-${aws_instance.myec2.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/id_ed25519 ansible_python_interpreter=/usr/bin/python3.9 ansible_ssh_common_args='-o IdentitiesOnly=yes'
+${aws_instance.myec2.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=${path.module}/web-key.pem ansible_python_interpreter=/usr/bin/python3.9 ansible_ssh_common_args='-o IdentitiesOnly=yes'
 EOF
-
-  filename = "${path.module}/inventory.ini"
 }
--- runplaybook.tf
+++ runplaybook.tf
@@ -1,4 +1,18 @@
+resource "null_resource" "wait_for_ssh" {
+  triggers = {
+    instance_ip = aws_instance.myec2.public_ip
+  }
+
+  provisioner "local-exec" {
+    command = "until ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=5 -i ${path.module}/web-key.pem ec2-user@${aws_instance.myec2.public_ip} 'echo ssh_ready' >/dev/null 2>&1; do echo 'Waiting for SSH...'; sleep 5; done"
+  }
+}
+
 resource "null_resource" "run_playbook" {
+  depends_on = [
+    null_resource.wait_for_ssh,
+    local_file.inventory
+  ]
 
   triggers = {
     instance_id = aws_instance.myec2.id
@@ -7,7 +21,6 @@ resource "null_resource" "run_playbook" {
 
   provisioner "local-exec" {
     working_dir = path.module
-    command     = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.ini playbook.yml"
+    command     = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook.yml"
   }
 }
-
